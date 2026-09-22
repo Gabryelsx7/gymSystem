c006e6ae5a85b5a0e3dbe70bf00f62eca4288d51
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-09-22 16:24:07 -0300

    feat:Tratamento execeções

---
+++ src/main/java/dev/gabryel/demo/exception/GlobalExcepetionHandler.java
@@ -0,0 +1,44 @@
+package dev.gabryel.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExcepetionHandler extends RuntimeException {
+    public GlobalExcepetionHandler(String message) {
+        super(message);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroResponse> tratarErroValidacao(MethodArgumentNotValidException ex){
+        List<String> mensagens =  ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": "+fieldError.getDefaultMessage())
+                .toList();
+
+        ErroResponse response =new ErroResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de validação",
+                mensagens
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+    @ExceptionHandler(RegraNegocioException.class)
+    public ResponseEntity<ErroResponse> regraNegocioException(RegraNegocioException ex){
+        ErroResponse response = new ErroResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de regra de negócio",
+                List.of(ex.getMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
---
+++ src/main/java/dev/gabryel/demo/exception/RegraNegocioException.java
@@ -0,0 +1,8 @@
+package dev.gabryel.demo.exception;
+
+public class RegraNegocioException extends RuntimeException{
+
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
